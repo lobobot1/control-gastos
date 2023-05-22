e11b7221d06eb278317b2b7cee3c096df14b8fe7
kevin <kevincolmenaresm@gmail.com>
2023-05-21 20:58:33 -0400

    add formatter function

--- src/components/ControlDePresupuesto.tsx
+++ src/components/ControlDePresupuesto.tsx
@@ -1,26 +1,35 @@
 interface Props {
-    presupuesto: number;
+  presupuesto: number;
 }
 
-const ControlDePresupuesto = ({presupuesto}:Props) => {
+const ControlDePresupuesto = ({ presupuesto }: Props) => {
+  const amount = (money: number): string => {
+    return money.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 0,
+    });
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <p>
-            Grafica aqui
-        </p>
+        <p>Grafica aqui</p>
 
         <div className="contenido-presupuesto">
-            <p>
-                <span>
-                    Restante: 
-                </span>
-                $ {presupuesto}
-            </p>
+          <p>
+            <span>Restante:</span>$ {amount(presupuesto)}
+          </p>
+          <p>
+            <span>Disponible:</span>$ {amount(0)}
+          </p>
+          <p>
+            <span>Gastado:</span>$ {amount(0)}
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ControlDePresupuesto
+export default ControlDePresupuesto;
